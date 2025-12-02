1ed5d291b009cb74dbeb703c6e7d87050ee78f95
ashwanikumar1809kr-cmd <ashwanikumar1809@gmail.com>
2025-12-03 03:39:38 +0530

    connected to mongodb server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const ServerConfig = require('./config/serverConfig');
+const connectDB = require('./config/dbConfig');
 
 const app = express();
 
-app.listen(ServerConfig.PORT, () => {
-    console.log(`server started at port ${ServerConfig.PORT}...!!`);
-})
+app.listen(ServerConfig.PORT,async () => {
+    await connectDB();
+    console.log(`server started at port ${ServerConfig.PORT}...!!`);    
+});
 
 
 //34.5.12.8:5500 -> socket  address for other laptop
