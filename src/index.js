f2ccd5029e72b0fb2e7a8fbf954089bec2dc1867
ashwanikumar1809kr-cmd <ashwanikumar1809@gmail.com>
2025-12-06 17:34:18 +0530

    Added body parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const ServerConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
+
+
+
 
 app.listen(ServerConfig.PORT,async () => {
     await connectDB();
@@ -12,4 +20,4 @@ app.listen(ServerConfig.PORT,async () => {
 
 
 //34.5.12.8:5500 -> socket  address for other laptop
-//localhost:5500-> socket address fpr own laptop
+//localhost:5500-> socket address fpr own laptop       
